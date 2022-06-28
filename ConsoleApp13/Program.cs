066dab3a2fc35d3ad1b63389af7a994aba245b1e
OsmanGurbuz <107862900+OsmanGurbuz@users.noreply.github.com>
2022-06-28 13:41:30 +0300

    Girilen harfleri alfabetik sıralma , girilen rakamları küçükten büyüğe
    Girilen harfleri alfabetik sıralma , girilen rakamları küçükten büyüğe sıralarken
    
    aynı harfden iki defa girilemez ve rakamlar harflerden sonra gelicek şekilde

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string a ;
+            object rakamlar;
+            
+            
+            var list = new List<string>();
+            
+            Console.WriteLine("Alfabetik sırslanması için önce harfleri giriniz-iki defa aynı harf girilemez-");
+            a = Console.ReadLine();
+            list.Add(a);
+            Console.WriteLine("Rakamları giriniz");
+            rakamlar = Console.ReadLine();
+            Console.WriteLine("----------------------------");
+            int uzunluk = a.Length;
+            list.Sort();
+            
+            Console.Write("Alfabetik sıralı hali :");
+            
+            
+            Console.WriteLine(list);
+            Console.WriteLine(rakamlar);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
